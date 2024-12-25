b2978239ea93849bed3407d851f9980b3e2fee05
tomspringmountain <fow0p.mgtmp@gmail.com>
2024-12-25 14:01:43 +0900

    kadai4-2

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -2,6 +2,7 @@ package raisetech.StudentManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
