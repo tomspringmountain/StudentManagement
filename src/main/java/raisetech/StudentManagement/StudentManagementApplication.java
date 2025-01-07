709a509f503dab58fe5de47eb95d09880b73b7ad
tomspringmountain <fow0p.mgtmp@gmail.com>
2025-01-07 19:47:44 +0900

    kadai5

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,22 +1,34 @@
 package raisetech.StudentManagement;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+  private Map<String,String> student =new HashMap<>();
+
   public static void main(String[] args) {
     SpringApplication.run(StudentManagementApplication.class, args);
   }
 
-  @GetMapping("/goodevening")
-  public String goodevening() {
-    return "Have a nice evening!";
+  @GetMapping("/studentInfo")
+  public Map<String,String> getstudentInfo() {
+    return student;
+  }
+
+  @PostMapping("/studentInfo")
+  public String addStudentInfo(@RequestParam String name, @RequestParam String age){
+    student.put(name, age);
+    return "tuika" + name + " " + age + "sai";
   }
 
 }
