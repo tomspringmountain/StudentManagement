00e76cc9df29b6d743c43fbe1f82e8c2df31a1cc
tomspringmountain <fow0p.mgtmp@gmail.com>
2025-04-15 20:24:02 +0900

    第23回演習課題

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -7,7 +7,6 @@ import jakarta.validation.constraints.Pattern;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.exception.TestException;
 import raisetech.StudentManagement.service.StudentService;
@@ -38,8 +36,7 @@ public class StudentController {
   }
 
   /**
-   * 受講生詳細一覧検索です。
-   * 全件検索を行うので、条件指定は行いません。
+   * 受講生詳細一覧検索です。 全件検索を行うので、条件指定は行いません。
    *
    * @return 受講生詳細一覧(全件)
    */
@@ -47,18 +44,17 @@ public class StudentController {
   public List<StudentDetail> getStudentList() throws TestException {
 //    throw new TestException(
 //        "現在このAPIは利用できません。URLは「studentList」ではなく「students」を利用してください。");
-   return service.searchStudentList();
+    return service.searchStudentList();
   }
 
   /**
-   * 受講生詳細の検索です。
-   * ＩＤに紐づく任意の受講生の情報を取得します。
+   * 受講生詳細の検索です。 ＩＤに紐づく任意の受講生の情報を取得します。
    *
-   * @param id　受講生ＩＤ
+   * @param id 　受講生ＩＤ
    * @return 受講生
    */
   @GetMapping("/student/{id}")
-  public StudentDetail setStudent(
+  public StudentDetail getStudent(
       @PathVariable @NotBlank @Pattern(regexp = "^\\d+$") String id) {
     return service.searchStudent(id);
   }
@@ -66,7 +62,7 @@ public class StudentController {
   /**
    * 受講生詳細の登録を行います。
    *
-   * @param studentDetail　受講生詳細
+   * @param studentDetail 　受講生詳細
    * @return 実行結果
    */
 
@@ -78,10 +74,9 @@ public class StudentController {
   }
 
   /**
-   * 受講生詳細の更新を行います。
-   * キャンセルフラグの更新もここで行います(論理削除)
+   * 受講生詳細の更新を行います。 キャンセルフラグの更新もここで行います(論理削除)
    *
-   * @param studentDetail　受講生詳細
+   * @param studentDetail 　受講生詳細
    * @return 実行結果
    */
   @PutMapping("/updateStudent")
@@ -91,7 +86,7 @@ public class StudentController {
     return ResponseEntity.ok("更新処理が成功しました。");
   }
 
-//  @ExceptionHandler(TestException.class)
+  //  @ExceptionHandler(TestException.class)
 //  public  ResponseEntity<String> handleTestException(TestException ex){
 //    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
 //  }
@@ -99,14 +94,14 @@ public class StudentController {
 //public ResponseEntity<String> ValidationException(String message) {
 //  return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("カタカナで入力してください。");
 //}
-@PostMapping("/testRegister")
-public ResponseEntity<String> register()
-    throws ValidationException {
-  // ここで独自のロジックによってエラーを投げることも可能
+  @PostMapping("/testRegister")
+  public ResponseEntity<String> register()
+      throws ValidationException {
+
 //  if (!student.getKanaName().matches("^[ァ-ヶー　]+$"))
     throw new ValidationException("フリガナはカタカナで入力してください。");
 
 //  return ResponseEntity.ok("登録成功しました。");
-}
+  }
 
 }
