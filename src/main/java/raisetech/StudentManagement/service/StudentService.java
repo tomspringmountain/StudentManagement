915f87315c87520d4c643d00a4820b53738e2441
tomspringmountain <fow0p.mgtmp@gmail.com>
2025-04-21 21:58:11 +0900

    第27回演習課題サービステスト

--- src/main/java/raisetech/StudentManagement/service/StudentService.java
+++ src/main/java/raisetech/StudentManagement/service/StudentService.java
@@ -68,7 +68,7 @@ public class StudentService {
 
     repository.registerStudent(student);
     for (StudentCourse studentCourse : studentDetail.getStudentCourseList()) {
-      initStudentsCourse(studentCourse, student);
+      initStudentsCourse(studentCourse, student.getId());
       repository.registerStudentCourse(studentCourse);
     }
     return studentDetail;
@@ -78,13 +78,13 @@ public class StudentService {
    * 受講生コース情報を登録する際の初期情報を設定する。
    *
    * @param studentCourse 受講生コース情報
-   * @param student 受講生
+   * @param id 受講生
    */
 
-  private void initStudentsCourse(StudentCourse studentCourse, Student student) {
+  void initStudentsCourse(StudentCourse studentCourse, String id) {
     LocalDateTime now = LocalDateTime.now();
 
-    studentCourse.setStudentId(student.getId());
+    studentCourse.setStudentId(id);
     studentCourse.setCourseStartAt(now);
     studentCourse.setCourseEndAt(now.plusYears(1));
   }
